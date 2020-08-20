5780238a7d85cd47f4cbc80cb70a3e4e61b25839
Viktor Kerkez <alefnula@gmail.com>
2020-08-20 15:35:16 +0100

    Add color picker for color field in admin interface.

--- django_tea/commands/__init__.py
+++ django_tea/commands/__init__.py
@@ -1,15 +1,3 @@
-__all__ = [
-    "config_app",
-    "auth_app",
-    "db_app",
-    "django_app",
-    "server_app",
-    "test_app",
-]
+__all__ = ["config_app"]
 
 from django_tea.commands.config import app as config_app
-from django_tea.commands.auth import app as auth_app
-from django_tea.commands.db import app as db_app
-from django_tea.commands.django_app import app as django_app
-from django_tea.commands.server import app as server_app
-from django_tea.commands.test import app as test_app
--- django_tea/models/mixins/colored.py
+++ django_tea/models/mixins/colored.py
@@ -1,16 +1,32 @@
 from typing import Optional
 
 from django.db import models
+from django.forms.widgets import TextInput
+from django.utils.html import mark_safe, SafeString
+
+
+class ColorField(models.CharField):
+    def __init__(self, *args, **kwargs):
+        # It should always be 7
+        kwargs["max_length"] = 7
+        kwargs.setdefault("default", "#FFFFFF")
+        super().__init__(*args, **kwargs)
+
+    def formfield(self, **kwargs):
+        kwargs["widget"] = TextInput(attrs={"type": "color"})
+        return super(ColorField, self).formfield(**kwargs)
 
 
 class ColoredMixin(models.Model):
-    color = models.CharField(
-        max_length=7, default="#000000", null=False, blank=False
-    )
+    color = ColorField(null=False, blank=False)
 
     def rich(self, s: Optional[str] = None) -> str:
-        """Return a rich markup for the provided string or color hex."""
+        """Return a rich colored markup for the provided string."""
         return f"[{self.color}]{s or self.color}[/{self.color}]"
+
+    def html(self, s: str) -> SafeString:
+        """Return a django safe string in current color."""
+        return mark_safe(f'<span style="color: {self.color}">{s}</span>')
 
     class Meta:
         app_label = "django_tea"
